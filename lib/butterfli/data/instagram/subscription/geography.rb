92b4765285134ccf5b3fce9f3f7df9d86024e570
David Elner <david@davidelner.com>
2015-06-30 21:59:22 -0400

    WIP: Geography recent media transformation

---
+++ lib/butterfli/data/instagram/subscription/geography.rb
@@ -0,0 +1,5 @@
+class Butterfli::Data::Instagram::Subscription::Geography < Butterfli::Data::Instagram::MediaObject
+  def transform
+    story = Butterfli::Story.new
+  end
+end
---
+++ spec/butterfli/data/instagram/subscription/geography_spec.rb
@@ -0,0 +1,7 @@
+require 'spec_helper'
+
+# describe Butterfli::Data::Instagram::Subscription::Geography do 
+#   context "created from a media object" do
+
+#   end
+# end
