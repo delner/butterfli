ce0d1539ac2019726a3dad7410ff42a2a98d3948
David Elner <david@davidelner.com>
2015-08-29 22:56:11 -0400

    Added: ID and source_key fields to story

--- lib/butterfli/story.rb
+++ lib/butterfli/story.rb
@@ -13,4 +13,11 @@ class Butterfli::Story < Hash
   field :type # The kind of story (e.g. :post, :photo, :video)
   field :source # Name of the source it came from (:twitter, :facebook)
   field :created_date # Time the story occurred
+
+  def id
+    self[:id] ||= SecureRandom.hex(8)
+  end
+  def source_key
+    Digest::SHA256.hexdigest "#{self.source.to_s}:#{self.references.source_type.to_s}:#{self.references.source_id.to_s}"
+  end
 end
