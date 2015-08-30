9e8a7b10e192fae5594bf4843970fce5ca6876e2
David Elner <david@davidelner.com>
2015-08-29 21:20:20 -0400

    Changed: Clarify couple of comments

--- lib/butterfli/story.rb
+++ lib/butterfli/story.rb
@@ -10,7 +10,7 @@ class Butterfli::Story < Hash
   include Butterfli::Imageable
   include Butterfli::Mappable
 
-  field :type # The kind of story (e.g. 'tweet', 'post', 'photo', 'video')
-  field :source # Name of the source it came from ('twitter', 'facebook')
+  field :type # The kind of story (e.g. :post, :photo, :video)
+  field :source # Name of the source it came from (:twitter, :facebook)
   field :created_date # Time the story occurred
 end
