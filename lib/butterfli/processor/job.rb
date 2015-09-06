49bf14f7c5775cf7c24f609c7baf6bf5a24fe968
David Elner <david@davidelner.com>
2015-09-06 15:15:07 -0400

    Fixed: Equality comparison on Job

--- lib/butterfli/processor/job.rb
+++ lib/butterfli/processor/job.rb
@@ -1,9 +1,12 @@
 class Butterfli::Job
   attr_accessor :args
   def initialize(args = {})
-    self.args = args
+    self.args = args.empty? ? (self.args ||= {}) : args
   end
   def hash
     [self.class.name, self.args].hash
+  end
+  def eql?(other)
+    self.hash == other.hash
   end
 end
--- spec/butterfli/processor/job_spec.rb
+++ spec/butterfli/processor/job_spec.rb
@@ -21,6 +21,7 @@ describe Butterfli::Job do
         let(:job_two) { job_class_one.new(value: 1) }
         it do
           expect(job_one.hash).to eq(job_two.hash)
+          expect(job_one.eql?(job_two)).to be true
         end
       end
       context "and different arguments" do
@@ -28,6 +29,7 @@ describe Butterfli::Job do
         let(:job_two) { job_class_one.new(value: 2) }
         it do
           expect(job_one.hash).to_not eq(job_two.hash)
+          expect(job_one.eql?(job_two)).to be false
         end
       end
     end
@@ -37,6 +39,7 @@ describe Butterfli::Job do
         let(:job_two) { job_class_two.new(value: 1) }
         it do
           expect(job_one.hash).to_not eq(job_two.hash)
+          expect(job_one.eql?(job_two)).to be false
         end
       end
       context "and different arguments" do
@@ -44,6 +47,7 @@ describe Butterfli::Job do
         let(:job_two) { job_class_two.new(value: 2) }
         it do
           expect(job_one.hash).to_not eq(job_two.hash)
+          expect(job_one.eql?(job_two)).to be false
         end
       end
     end
