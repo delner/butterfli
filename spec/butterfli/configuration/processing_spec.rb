1cc8828dd379a4bbf358ada13d56d3eda0ccfd9f
David Elner <david@davidelner.com>
2015-09-20 11:09:31 -0400

    Added: Some missing specs

---
+++ spec/butterfli/configuration/processing_spec.rb
@@ -0,0 +1,6 @@
+require 'spec_helper'
+
+describe Butterfli::Configuration::Processing do
+  # TODO: Fill this out with more detail
+  it { expect(Butterfli::Configuration::Processing).to_not be_nil }
+end
---
+++ spec/butterfli/configuration/provisioning_spec.rb
@@ -0,0 +1,6 @@
+require 'spec_helper'
+
+describe Butterfli::Configuration::Provisioning do
+  # TODO: Fill this out with more detail
+  it { expect(Butterfli::Configuration::Provisioning).to_not be_nil }
+end
